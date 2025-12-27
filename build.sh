72e3e79f392aafccb4f7adbd1c7d4f7f55ccd455
TYNBUM <tynbum519@gmail.com>
2025-12-27 20:59:29 +0800

    Add files via upload

--- build.sh
+++ build.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 set -e
 
+export PLAYWRIGHT_BROWSERS_PATH=/opt/render/project/.cache/ms-playwright
+
 pip install -r requirements.txt
+
 python -m playwright install chromium
-python -m playwright install chromium chromium-headless-shell
+python -m playwright install chromium-headless-shell
